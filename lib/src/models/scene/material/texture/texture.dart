6d87edcdf74bd84b40262148a21d95d98eff69da
Jamie Kerber <kerber.jg@gmail.com>
2025-01-31 17:05:06 +0100

    Refactored `Texture` and `TextureSampler`

--- lib/src/models/scene/material/texture/texture.dart
+++ lib/src/models/scene/material/texture/texture.dart
@@ -1,8 +1,7 @@
-import 'package:playx_3d_scene/src/models/scene/material/texture/enums/texture_type.dart';
 import 'package:playx_3d_scene/src/models/scene/material/texture/texture_sampler.dart';
 
 /// An object represents textures to be loaded by the material.
-class PlayxTexture {
+class Texture {
   /// asset path of the texture.
   String? assetPath;
 
@@ -13,29 +12,29 @@ class PlayxTexture {
   TextureType? type;
 
   ///
-  PlayxTextureSampler? sampler;
+  TextureSampler? sampler;
 
-  PlayxTexture.asset(this.assetPath, {this.type, this.sampler});
+  Texture.asset(this.assetPath, {this.type, this.sampler});
 
-  PlayxTexture.url(this.url, {this.type, this.sampler});
+  Texture.url(this.url, {this.type, this.sampler});
 
-  Map<String, dynamic> toJson() => {
-        'assetPath': assetPath,
-        'url': url,
-        'type': type?.toName(),
-        'sampler': sampler?.toJson(),
-      };
+  Map<String, dynamic> toJson() => <String, dynamic>{
+    'assetPath': assetPath,
+    'url': url,
+    'type': type?.value,
+    'sampler': sampler?.toJson(),
+  };
 
   @override
   String toString() {
-    return 'PlayxTexture(assetPath: $assetPath, url: $url, type: $type, sampler: $sampler)';
+    return 'Texture(assetPath: $assetPath, url: $url, type: $type, sampler: $sampler)';
   }
 
   @override
-  bool operator ==(Object other) {
+  bool operator ==(final Object other) {
     if (identical(this, other)) return true;
 
-    return other is PlayxTexture &&
+    return other is Texture &&
         other.assetPath == assetPath &&
         other.url == url &&
         other.type == type &&
--- lib/src/models/scene/material/texture/texture_sampler.dart
+++ lib/src/models/scene/material/texture/texture_sampler.dart
@@ -1,9 +1,87 @@
-import 'package:playx_3d_scene/src/models/scene/material/texture/enums/mag_filter.dart';
-import 'package:playx_3d_scene/src/models/scene/material/texture/enums/min_filter.dart';
-import 'package:playx_3d_scene/src/models/scene/material/texture/enums/wrap_mode.dart';
+/// Magnification filter to be used.
+///
+/// See Also:
+/// [TextureSampler]
+enum MagFilter {
+  /// No filtering. Nearest neighbor is used.
+  nearest("NEAREST"),
+
+  /// Box filtering. Weighted average of 4 neighbors is used.
+  linear("LINEAR");
+
+  final String value;
+  const MagFilter(this.value);
+}
+
+
+
+/// Minification filter to be used.
+///
+/// See Also:
+/// [TextureSampler]
+enum MinFilter {
+  /// No filtering. Nearest neighbor is used.
+  nearest("NEAREST"),
+
+  /// Box filtering. Weighted average of 4 neighbors is used.
+  linear("LINEAR"),
+
+  /// Mip-mapping is activated. But no filtering occurs.
+  nearestMipmapNearest("NEAREST_MIPMAP_NEAREST"),
+
+  /// Box filtering within a mip-map level.
+  linearMipmapNearest("LINEAR_MIPMAP_NEAREST"),
+
+  /// Mip-map levels are interpolated, but no other filtering occurs.
+  nearestMipmapLinear("NEAREST_MIPMAP_LINEAR"),
+
+  /// Both interpolated Mip-mapping and linear filtering are used.
+  linearMipmapLinear("LINEAR_MIPMAP_LINEAR");
+
+  final String value;
+  const MinFilter(this.value);
+}
+
+
+
+
+///Type of the texture to be used
+/// Color is the only type of texture we want to pre-multiply with the alpha channel
+/// Pre-multiplication is the default behavior, so we need to turn it off  based on the type.
+enum TextureType {
+  color("COLOR"),
+  normal("NORMAL"),
+  data("DATA");
+
+  final String value;
+  const TextureType(this.value);
+}
+
+
+
+///Wrap Mode to be used.
+///
+///See Also:
+///[TextureSampler]
+enum WrapMode {
+  /// The edge of the texture extends to infinity.
+  clampToEdge("CLAMP_TO_EDGE"),
+
+  /// The texture infinitely repeats in the wrap direction.
+  repeat("REPEAT"),
+
+  /// The texture infinitely repeats and mirrors in the wrap direction.
+  mirroredRepeat("MIRRORED_REPEAT");
+
+  final String value;
+  const WrapMode(this.value);
+}
+
+
+
 
 /// An object that defines how a texture is accessed.
-class PlayxTextureSampler {
+class TextureSampler {
   ///Minification filter to be used.
   /// Defaults to LINEAR_MIPMAP_LINEAR
   MinFilter min;
@@ -19,33 +97,35 @@ class PlayxTextureSampler {
   ///Amount of anisotropy, controls anisotropic filtering, should be a power-of-two. The default is 0. The maximum permissible value is 7.
   double? anisotropy;
 
-  PlayxTextureSampler(
-      {this.min = MinFilter.linearMipmapLinear,
-      this.mag = MagFilter.linear,
-      this.wrap = WrapMode.clampToEdge,
-      this.anisotropy});
+  TextureSampler({
+    this.min = MinFilter.linearMipmapLinear,
+    this.mag = MagFilter.linear,
+    this.wrap = WrapMode.clampToEdge,
+    this.anisotropy,
+  });
 
-  Map<String, dynamic> toJson() => {
-        'min': min.toName(),
-        'mag': mag.toName(),
-        'wrap': wrap.toName(),
-        'anisotropy': anisotropy,
-      };
+  Map<String, dynamic> toJson() => <String, dynamic>{
+    'min': min.value,
+    'mag': mag.value,
+    'wrap': wrap.value,
+    'anisotropy': anisotropy,
+  };
 
   @override
   String toString() {
-    return 'PlayxTextureSampler(min: $min, mag: $mag, wrap: $wrap, anisotropy: $anisotropy)';
+    return 'TextureSampler(min: $min, mag: $mag, wrap: $wrap, anisotropy: $anisotropy)';
   }
 
   @override
-  bool operator ==(Object other) {
+  bool operator ==(final Object other) {
     if (identical(this, other)) return true;
 
-    return other is PlayxTextureSampler &&
-        other.min == min &&
-        other.mag == mag &&
-        other.wrap == wrap &&
-        other.anisotropy == anisotropy;
+    return other is TextureSampler &&
+      other.min == min &&
+      other.mag == mag &&
+      other.wrap == wrap &&
+      other.anisotropy == anisotropy
+    ;
   }
 
   @override
